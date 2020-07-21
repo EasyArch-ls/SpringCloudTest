23aa0cdc1a9d6a9fca95aadb9ac6ff00697b5037
lisheng <lisheng02@megvii.com>
2020-07-21 13:36:11 +0800

    zuul增加服务过滤功能

---
+++ service-zuul/src/main/java/spring/cloud/servicezuul/filter/MyFilter.java
@@ -0,0 +1,64 @@
+package spring.cloud.servicezuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/***
+ * @Author: lisheng
+ * @Date: 2020/7/21
+ * @Time: 下午1:14
+ * @Description:
+ * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+ *       pre：路由之前
+ *       routing：路由之时
+ *       post： 路由之后
+ *       error：发送错误调用
+ * filterOrder：过滤的顺序
+ * shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+ * run：过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问
+ ***/
+@Component
+@Slf4j
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context=RequestContext.getCurrentContext();
+        HttpServletRequest request=context.getRequest();
+        log.info(String.format("%s >>>>> %S",request.getMethod(),request.getRequestURL().toString()));
+        Object token=request.getParameter("token");
+        if (token==null){
+            log.warn("没有token");
+            context.setResponseStatusCode(401);
+            context.setSendZuulResponse(false);
+            try {
+                context.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
