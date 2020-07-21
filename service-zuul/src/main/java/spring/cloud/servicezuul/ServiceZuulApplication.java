57a607f2d5abb27452b69d1330cacfbc710a0195
lisheng <lisheng02@megvii.com>
2020-07-21 16:06:41 +0800

    补提交

---
+++ service-zuul/src/main/java/spring/cloud/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,18 @@
+package spring.cloud.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
---
+++ service-zuul/src/test/java/spring/cloud/servicezuul/ServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.servicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
