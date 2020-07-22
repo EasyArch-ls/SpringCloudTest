de5c7db432f5349676fcc23bd64fc4c175ddc944
lisheng <lisheng02@megvii.com>
2020-07-22 14:45:07 +0800

    实现配置中心总线刷新（rabbitmq）

--- config-client/src/main/java/spring/cloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/spring/cloud/configclient/ConfigClientApplication.java
@@ -3,13 +3,23 @@ package spring.cloud.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
+    /*@Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
+    }*/
 }
